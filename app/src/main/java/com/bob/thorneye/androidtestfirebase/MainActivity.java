14fee49e2b8507212381b9f00a6274f9a33c5794
bob <robin.lilius@gmail.com>
2016-09-16 13:23:08 +0200

    Managed to save data to firebase database

--- app/src/main/java/com/bob/thorneye/androidtestfirebase/MainActivity.java
+++ app/src/main/java/com/bob/thorneye/androidtestfirebase/MainActivity.java
@@ -1,13 +1,30 @@
 package com.bob.thorneye.androidtestfirebase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FIREBASE_URL = "https://bobstestproject1.firebaseio.com/";
+
+    private Firebase mFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
+
+        setUpFirebase();
+    }
+
+    private void setUpFirebase() {
+        mFirebase = new Firebase(FIREBASE_URL);
+
+        mFirebase.child("users").setValue(123);
+
+        System.out.println("FIREBASE SETUP IS FINISHED");
     }
 }
